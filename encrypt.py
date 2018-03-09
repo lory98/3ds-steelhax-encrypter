2bc2c5b587a610de331451df6cd5273365d10ee4
lory98 <36423090+lory98@users.noreply.github.com>
2018-03-09 21:25:41 +0100

    Encryption should now work properly

--- encrypt.py
+++ encrypt.py
@@ -12,12 +12,14 @@ def main():
     f = open(sys.argv[1], 'rb')
     disa = io.BytesIO(f.read())
     f.close()
+    game_id = int(sys.argv[2], 16)
     disa.seek(0x100, 0)
     header = disa.read(0x100)
-    digest = hashlib.sha256(disa_extract.getDigestBlock("sd", 0x00040000000d7e00, header)).digest()
+    digest = hashlib.sha256(disa_extract.getDigestBlock("sd", game_id, header)).digest()
     disa.seek(0x00, 0)
     disa.write(cmac.AesCmac(digest, keyEngine.getKeySdNandCmac()))
-    disa = disa_extract.cryptoUnwrap(disa, "sd", 0x00040000000d7e00, keyEngine.getKeySdDecrypt())   
+    disa.seek(0x00, 0)
+    disa = disa_extract.cryptoUnwrap(disa, "sd", game_id, keyEngine.getKeySdDecrypt())   
     with open("./out/00000001.sav", 'wb') as save:
         save.write(disa.getbuffer())
 
